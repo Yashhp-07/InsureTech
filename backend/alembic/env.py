cb60275c210b77e4ee61b187b04a08af9ba9e83f
meet <meetvora.inexture@gmail.com>
2026-06-12 15:44:43 +0530

    feat: Update database URL handling and switch to asyncpg for PostgreSQL

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,8 +8,11 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from app.core.base import Base
 from app.core.config import settings
 
+
+from app.modules.auth import models
+
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.database_url.replace("%", "%%"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
